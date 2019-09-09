d1e06e44338442a0d933a4aa4e173bcdb3511983
SB49849 <SB49849@SHELW42SLPC0883.ind.zensar.com>
2019-09-09 14:22:39 +0530

    Push

--- ShellTest/src/main/java/com/Shell/qa/Utility/TestUtil.java
+++ ShellTest/src/main/java/com/Shell/qa/Utility/TestUtil.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
