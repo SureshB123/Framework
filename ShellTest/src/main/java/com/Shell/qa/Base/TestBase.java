97344da6f557d78b103a06185270876c91c779ed
SB49849 <SB49849@SHELW42SLPC0883.ind.zensar.com>
2019-09-16 12:47:00 +0530

    Commit Changes

--- ShellTest/src/main/java/com/Shell/qa/Base/TestBase.java
+++ ShellTest/src/main/java/com/Shell/qa/Base/TestBase.java
@@ -29,7 +29,7 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class TestBase {
 	
-	//Initialize webdriver
+	//Initialize your webdriver
 	public static WebDriver driver;
 	
 	//Initializing properties file
