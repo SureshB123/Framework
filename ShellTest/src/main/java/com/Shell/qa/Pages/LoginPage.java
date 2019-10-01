f9f25f152342636e036eea4038ac746234921eb4
SB49849 <SB49849@SHELW42SLPC0883.ind.zensar.com>
2019-10-01 11:54:17 +0530

    Push Changes

--- ShellTest/src/main/java/com/Shell/qa/Pages/LoginPage.java
+++ ShellTest/src/main/java/com/Shell/qa/Pages/LoginPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.Shell.qa.Base.TestBase;
-import com.Shell.qa.Utility.TestUtil;
 
 public class LoginPage extends TestBase {
 	
---
+++ ShellTest/src/main/java/com/Shell/qa/Pages/OrderCardPage.java
@@ -0,0 +1,10 @@
+package com.Shell.qa.Pages;
+
+import com.Shell.qa.Base.TestBase;
+
+public class OrderCardPage extends TestBase {
+	
+	
+	
+
+}
